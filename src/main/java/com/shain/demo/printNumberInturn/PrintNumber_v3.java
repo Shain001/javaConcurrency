525e4e7a0df5c7e468c02125cd8b312cb303a3fb
Shain001 <84569049+Shain001@users.noreply.github.com>
2023-06-18 21:32:38 +1000

    add demo

--- src/main/java/com/shain/demo/printNumberInturn/PrintNumber_v3.java
+++ src/main/java/com/shain/demo/printNumberInturn/PrintNumber_v3.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * print numbers in turn using three threads, strictly order in A, B, C
- *
+ * <p>
  * this class uses retreentLock to implement since it's easier and clearer
  * Using object as a lock can also do the same thing, but it's more inconvenient, since you can't specify which threads
  * to wake up,
@@ -29,15 +29,13 @@ public class PrintNumber_v3 {
         @Override
         public void run() {
             try {
-                lock.lock();
                 while (num < 30) {
+                    lock.lock();
                     // 释放所， 进入await 状态
                     condition.await();
-                    // 这里需要重复判断， 因为否则会打印出31。 因为所有线程都在line35出await， 这意味着他们已经进入了while循环，
+                    // 如果将 lock.lock写在while 循环外，则这里需要重复判断 num是否大于30， 否则会打印出31。 因为所有线程都在line35出await， 这意味着他们已经进入了while循环，
                     // 所以就算此时num已经大于了29， 还是会打印出30/31
-                    if (num < 30) {
-                        System.out.println(Thread.currentThread().getName() + " " + num++);
-                    }
+                    System.out.println(Thread.currentThread().getName() + " " + num++);
                     nextCondition.signal();
                 }
 
@@ -53,7 +51,7 @@ public class PrintNumber_v3 {
      * 程序启动时， ABC均会进入 printer的run()， 并且会开始争抢lock。
      * at the very first time, 不一定是ABC中的哪个获得锁， 但是无论是哪一个获得了锁， 由于有condition.await()的存在，
      * 他都会让自身休眠。也即， 在程序刚刚开始， 走到 main 中的conditionA.signal之前， 没有线程能够执行run（）
-     *
+     * <p>
      * 之后， 在main 中的 lock.lock, 也不一定是哪个线程获得这个锁， 但是不论谁获得， 都通知A线程被唤醒。 此时，所有线程都在run方法中await，
      * A被唤醒之后则可开始执行， 然后依次唤醒下一个线程。
      *
